b29f54eb764b3436b8eac015c20688f002636de2
houle <theo.houle@epfl.ch>
2021-02-28 19:35:35 +0100

    mpu

--- MainProgram/mpu6050.cpp
+++ MainProgram/mpu6050.cpp
@@ -1,12 +1,12 @@
 #include "mpu6050.h"
-#include <Adafruit_MPU6050.h>
+#include <MPU6050_light.h>
 #include "Arduino.h"
 
-Adafruit_MPU6050 mpuAda;
+MPU6050 mpuAda(Wire);
 
+#define radToDeg 57.2958
 
-Mpu6050::Mpu6050() : Sensor(0x76) {}
-sensors_event_t acc, gyro, temp;
+Mpu6050::Mpu6050() : Sensor(0x68) {}
 double gXOff, gYOff, gZOff, aXOff, aYOff, aZOff;
 double currTime, prevTime, deltaTime;
 // Ratio to convert radians to degrees.
@@ -14,62 +14,69 @@ double currTime, prevTime, deltaTime;
 
 bool Mpu6050::begin(){
     // Wake up the mpu
-    bool succ = mpuAda.begin(i2cAddress);
-
-    //Set settings of mpu
-    mpuAda.setAccelerometerRange(MPU6050_RANGE_8_G);
-    mpuAda.setGyroRange(MPU6050_RANGE_2000_DEG);
-    mpuAda.setFilterBandwidth(MPU6050_BAND_94_HZ);
+    mpuAda.begin();
 
     degX, degY, degZ = 0;
+    velX, velY, velZ = 0;
 
-    getOffsets();
-
-    currTime = millis();
+    mpuAda.calcGyroOffsets();
 }
 
 // Get the most recent values and stores them.
 void Mpu6050::measure(){
+
+    /*
     //read the most recent values
-    mpuAda.getEvent(&acc, &gyro, &temp);
+    mpuAda.getEvent(&acc, &g, &temp);
 
     // Adjust gyro according to offset
-    double gX = gyro.gyro.x;
+    double gX = g.gyro.x;
     double gXTrue = gX - gXOff;
-    double gYTrue = gyro.gyro.y - gYOff;
-    double gZTrue = gyro.gyro.z - gZOff;
+    double gYTrue = g.gyro.y - gYOff;
+    double gZTrue = g.gyro.z - gZOff;
     // Adjust the accelerometer value according to the offset
     double aXTrue = acc.acceleration.x;
     double aYTrue = acc.acceleration.y;
     double aZTrue = acc.acceleration.z;
-
+    //Serial.print(gX); Serial.print(" ");
+    //Serial.print(gXTrue); Serial.print(" ");
     // Get the delta time since last measure
     prevTime = currTime;
     currTime = millis();
     deltaTime = (currTime - prevTime) / 1000; //divide by 1000 to get seconds
-    Serial.print(gX); Serial.print(" ");
-    Serial.print(gXTrue); Serial.print(" ");
     // Increment orientation values
-    degX = degX + (gXTrue * deltaTime * RAD_TO_DEG);
-    degY = degY + gYTrue * deltaTime * RAD_TO_DEG;
-    degZ = degZ + gZTrue * deltaTime * RAD_TO_DEG;
+    degX = degX + gXTrue * deltaTime * radToDeg;
+    //Serial.print(degX); Serial.println(" ");
+    degY = degY + gYTrue * deltaTime * radToDeg;
+    degZ = degZ + gZTrue * deltaTime * radToDeg;
     // Increment velocity values
     velX += aXTrue * deltaTime;
     velY += aYTrue * deltaTime;
     velZ += aZTrue * deltaTime;
-    Serial.println(degX);
+    */
+    mpuAda.update();
+    
+    degX = mpuAda.getAngleX();
+    degY = mpuAda.getAngleY();
+    degZ = mpuAda.getAngleZ();
+
+    velX = mpuAda.getAccX();
+    velY = mpuAda.getAccY();
+    velZ = mpuAda.getAccZ();
+    //TODO change to velocity
 }
 
 // Compute offset by averarging over a 100 measures.
 void Mpu6050::getOffsets(){
+    /**
     gXOff, gYOff, gZOff = 0;
     aXOff, aYOff, aZOff = 0;
     for(int i =0; i<100; i++){
-        mpuAda.getEvent(&acc, &gyro, &temp);
+        mpuAda.getEvent(&acc, &g, &temp);
 
-        gXOff += gyro.gyro.x;
-        gYOff += gyro.gyro.y;
-        gZOff += gyro.gyro.z;
+        gXOff += g.gyro.x;
+        gYOff += g.gyro.y;
+        gZOff += g.gyro.z;
 
         aXOff += acc.acceleration.x;
         aYOff += acc.acceleration.y;
@@ -79,6 +86,7 @@ void Mpu6050::getOffsets(){
     gXOff /= 100; gYOff /= 100; gZOff /= 100; 
     aXOff /= 100; aYOff /= 100; aZOff /= 100; 
     Serial.print("G offsets: "); Serial.print(gXOff); Serial.print(" "); Serial.print(gYOff); Serial.print(" "); Serial.println(gZOff);
+    */
 }
 
 //Getter for the velocity and orientation
--- MainProgram/sensor.h
+++ MainProgram/sensor.h
@@ -10,10 +10,9 @@ class Sensor{
 
 public:
    Sensor(int i2cAddress) : i2cAddress(i2cAddress) {};
-
+   virtual void measure() = 0; // these need to be overwritten
 protected:
   int i2cAddress;
-  virtual void measure() = 0; // these need to be overwritten
 private:
 };
 
