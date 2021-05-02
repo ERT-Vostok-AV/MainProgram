936d34f08fdaa8316eccb5b382dba6c8881f271e
unknown <theo.houle@epfl.ch>
2021-05-02 12:26:09 +0200

    updated event manager

--- MainProgram/eventManager.cpp
+++ MainProgram/eventManager.cpp
@@ -5,43 +5,51 @@
 	- Vitesse : m/s
 */
 
-const double LIFTOFF_ALTITUDE = 10;			// Altitude minimum pour detecter le decollage
-const double LIFTOFF_SPEED = 10;		// Vitesse minimum pour le detection du decollage
-
-// Idee c'est d'etre sur que la vitesse verticale est bien negatif, et donc que l'apogee est passe
-// En gros on va comparer maxAlt et l'altitude actuel, si la difference est plus grande que cette valeur alors il detecte que la fusee redescend.
-const double APOGEE_DETECTION_THRESHOLD = 3;	  
-const double RE_TRIGGER_ALTITUDE = 300;
-const double TOUCHDOWN_MAX_ALTITUDE = 20;		// Si il n'est pas descendu en dessous de cette altitude il ne peut pas declarer Touchdown (retourner true)
-const double TOUCHDOWN_MAX_SPEED = 1/2;	
+#define LIFT_OFF 10.0
+#define APOGEE_DETECTION_THRESHOLD 3
+#define RE_TRIGGER_ALTITUDE 300
+#define TOUCHDOWN_MAX_ALTITUDE 20
+#define STAGNATION_REPETITION_COUNT 10
+#define STAGNATION_DIFF_THRESHOLD 0.3
 
 EventManager::EventManager() : triggered(false) {}
 
-bool EventManager::isLiftOff(double alt, double velZ){
+bool EventManager::isLiftOff(double alt, double accelZ){
 	bool data1 = alt > LIFTOFF_ALTITUDE;
-	bool data2 = velZ > LIFTOFF_SPEED;
-	return data1 or data2;	// and ou or ?
+	//bool data2 = accelZ > LIFTOFF_SPEED; // To keep or not to keep ?
+	return data1; // or data2;	// and ou or ?
 }
 
-bool EventManager::isApogee(double alt, double velZ){
+bool EventManager::isApogee(double alt, double accelZ){
 	if (alt > maxAlt){
 	  maxAlt = alt;
 	}
-	bool data1 = velZ < 0;					// detecte vitesse verticale negative
+	//bool data1 = accelZ < 0;					// detecte vitesse verticale negative
 	bool data2 = (maxAlt - alt) > APOGEE_DETECTION_THRESHOLD;	// detecte altitude qui redescent
-	return data1 and data2; // and ou or?
+	return data2; // or data1
 	}
 
 bool EventManager::isReTrigger(double alt){
 	return alt <= RE_TRIGGER_ALTITUDE; 
 }
 
-bool EventManager::isTouchDown(double alt, double velZ){
-	bool data1 = alt < RE_TRIGGER_ALTITUDE;								// verifie que c'est apres la descente
-	bool data2 = abs(velZ) < TOUCHDOWN_MAX_SPEED;
-  // Peut etre verifie si stagne (buffer ?)
- 
-	return (data2 and data1);
+bool EventManager::isTouchDown(double alt, double accelZ){
+  bool res = false;
+  if(lastAlt != 0){
+    if(abs(lastAlt - alt) <= STAGNATION_DIFF_THRESHOLD ){
+      stagnation_count += 1;
+      if(stagnation_count >= 5){ // ou 10 ?
+        res = true;
+      }
+      else{
+        res = false;
+      }
+    } else {
+      stagnation_count = 0;
+    }
+    lastAlt = alt;
+  }
+	return data1; // or data2
 }
 
 void EventManager::trigger(){
--- MainProgram/eventManager.h
+++ MainProgram/eventManager.h
@@ -12,17 +12,16 @@ class EventManager {
 public:
     EventManager();
 
-    bool isLiftOff(double alt, double velZ);
-    bool isTouchDown(double alt, double velZ);
-    bool isApogee(double alt, double velZ);
+    bool isLiftOff(double alt, double accelZ);
+    bool isTouchDown(double alt, double accelZ);
+    bool isApogee(double alt, double accelZ);
     bool isReTrigger(double alt);
     void trigger();
     bool hasTriggered();
 private:
     bool triggered;
-	double maxAlt = 0;
+	  double maxAlt = 0;
+    int stagnation_count = 0;
+    double lastAtl = 0;
 };
-
-
-
 #endif // EVENT_MANAGER_H
