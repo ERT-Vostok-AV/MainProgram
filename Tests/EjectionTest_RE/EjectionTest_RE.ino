327fe1b64c0e7e83b0054446c463c609fdd11672
unknown <theo.houle@epfl.ch>
2021-05-04 23:10:33 +0200

    added ejection test for RE

---
+++ Tests/EjectionTest_RE/EjectionTest_RE.ino
@@ -0,0 +1,37 @@
+#define led 13
+#define EMPIN 10
+
+#define burnTime 1000 // ms
+#define timeout 15000 // ms
+
+void setup() {
+    pinMode(EMPIN, OUTPUT);
+    pinMode(led, OUTPUT);
+
+    digitalWrite(led, HIGH);
+    delay(2000);
+    digitalWrite(led, LOW);
+    delay(1000);
+
+    int cnt = 0;
+    while(cnt < 5){
+      flash(100);
+      delay(1000);
+      cnt += 1;
+    }
+
+    digitalWrite(EMPIN, HIGH);
+    delay(burnTime);
+    digitalWrite(EMPIN, LOW);
+    digitalWrite(led, HIGH);
+    
+}
+
+void flash(int duration){
+  digitalWrite(led, HIGH);
+  delay(duration);
+  digitalWrite(led, LOW);
+}
+
+void loop() {
+}
